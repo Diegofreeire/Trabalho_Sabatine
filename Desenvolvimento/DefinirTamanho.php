2e89b3ef6cbd36586a860ac1c9041536bb86c606
Seidjy <samuelpadovangit config --global user.name@hotmail.com>
2017-05-09 00:58:37 -0300

    Implementado linha de Z

--- Desenvolvimento/DefinirTamanho.php
+++ Desenvolvimento/DefinirTamanho.php
@@ -1,18 +1,18 @@
 <form method="POST" action="SimplexMaximizando.php">
-	<?php 
+	<?php
 require_once("Principal.php");
 	echo "Z = ";
-	for ($i=0; $i < $_POST["variaveis"]; $i++) { 
+	for ($i=0; $i < $_POST["variaveis"]; $i++) {
 			echo "<input type='text' name='x$i' id='x$i'> x$i";
 	}
 
 	echo "</br>Restrições</br>";
 
-	for ($i=0; $i < $_POST["restricoes"]; $i++) { 
-		for ($j=0; $j < $_POST["variaveis"]; $j++) { 
+	for ($i=0; $i < $_POST["restricoes"]; $i++) {
+		for ($j=0; $j < $_POST["variaveis"]; $j++) {
 			echo "<input type='text' name='x$i$j'> x$j";
 		}
-			echo "<= <input type='text' name='b$i$j' id='b$i%j'> </br>";
+			echo "<= <input type='text' name='b$i' id='b$i' > </br>";
 	}
  ?>
 <input type="hidden" name="variaveis" value="<?=$_POST['variaveis']?>">
--- Desenvolvimento/SimplexMaximizando.php
+++ Desenvolvimento/SimplexMaximizando.php
@@ -1,22 +1,37 @@
 <?php 
 $vetor = array();
-for ($i=0; $i < $_POST['restricoes']; $i++) { 
+for ($i=0; $i < $_POST['restricoes']; $i++) {//pegar os valores das restrições
 	for ($j=0; $j < $_POST["variaveis"]; $j++) {
 		$vetor["x$j"]["$i"] = $_POST["x$i$j"];
-	}	
+	}
 }
 
-for ($i=count($vetor), $n = 0; $n < $_POST['restricoes']; $i++, $n++) { 
+$j;
+for ($i=count($vetor), $n = 0; $n < $_POST['restricoes']; $i++, $n++) { //gerar valores das variaveis de folga
 	for ($j=0; $j < $_POST["restricoes"]; $j++) {
 		if ($j == $n) {
 			$vetor["x$i"]["$j"] = 1;
 		}else{
 			$vetor["x$i"]["$j"] = 0;
-		}		
+		}
 	}
 }
+
+for ($i=0; $i < $_POST['restricoes']; $i++) {//pegando valores da coluna b
+	$vetor["b"]["$i"] = $_POST["b$i"];
+}
+
 $tamanhoVetor = (count($vetor));
-for ($i=0; $i < $_POST['restricoes']; $i++) { 
+
+for ($i=0; $i < $_POST["variaveis"]; $i++) { //pegar valores de z
+	$vetor["z"]["$i"] = $_POST["x$i"];
+}
+
+for ($i=count($vetor["z"]); $i < $tamanhoVetor; $i++) { //completar as colunas valores de Z com 0
+	$vetor["z"]["$i"] = 0;
+}	
+
+for ($i=0; $i < $_POST['restricoes']; $i++) { //gerando as linhas
 	for ($j=0; $j < $_POST["restricoes"]; $j++) {
 		for ($k=0; $k < $tamanhoVetor; $k++) {
 			if ($j == $i) {
@@ -26,7 +41,7 @@ for ($i=0; $i < $_POST['restricoes']; $i++) {
 	}	
 }
 
-$a = min($vetor["l3"]);
+$a = min($vetor["z"]);
 
 $continuar = true;
 //while($continuar){
