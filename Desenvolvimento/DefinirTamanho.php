dcf4daf64da59c5c212be27e344561d823954f44
Seidjy <samuelpadovangit config --global user.name@hotmail.com>
2017-05-10 20:56:28 -0300

    Commit do diego

--- Desenvolvimento/DefinirTamanho.php
+++ Desenvolvimento/DefinirTamanho.php
@@ -1,25 +1,28 @@
-<form method="POST" action="SimplexMaximizando.php">
-	<?php
-require_once("Principal.php");
-	echo "Z = ";
-	for ($i=0; $i < $_POST["variaveis"]; $i++) {
-			echo "<input type='text' name='x$i' id='x$i'> x$i";
-	}
+<?php
+	include"Principal.html";
+?>
+	<div class="container">
+		<section class="tabelaRestricao">
+			<form method="POST" action="SimplexMaximizando.php">
+				<?php
+				echo "Z = ";
+				for ($i=0; $i < $_POST["variaveis"]; $i++) {
+						echo "<input type='text' name='x$i' id='x$i'> x$i";
+				}
 
-	echo "</br>Restrições</br>";
-
-	for ($i=0; $i < $_POST["restricoes"]; $i++) {
-		for ($j=0; $j < $_POST["variaveis"]; $j++) {
-			echo "<input type='text' name='x$i$j'> x$j";
-		}
-			echo "<= <input type='text' name='b$i' id='b$i' > </br>";
-	}
- ?>
-<input type="hidden" name="variaveis" value="<?=$_POST['variaveis']?>">
-<input type="hidden" name="restricoes" value="<?=$_POST['restricoes']?>">
- <input type="submit" name="" class="btn btn-primary">
-</form>
-
-
+				echo "</br>Restrições</br>";
+					for ($i=0; $i < $_POST["restricoes"]; $i++) {
+						for ($j=0; $j < $_POST["variaveis"]; $j++) {
+							echo "<input type='text' name='x$i$j'> x$j";
+						}
+							echo "<= <input type='text' name='b$i' id='b$i' > </br>";
+					}
+			 ?>
+			<input type="hidden" name="variaveis" value="<?=$_POST['variaveis']?>">
+			<input type="hidden" name="restricoes" value="<?=$_POST['restricoes']?>">
+			 <input type="submit" name="" class="btn btn-primary">
+			</form>
+		</section>
+	</div>
 </body>
 </html> 
--- Desenvolvimento/SimplexMaximizando.php
+++ Desenvolvimento/SimplexMaximizando.php
@@ -17,14 +17,14 @@ for ($i=count($vetor), $n = 0; $n < $_POST['restricoes']; $i++, $n++) { //gerar 
 	}
 }
 
+$tamanhoVetor = (count($vetor));//pegar o tamanho do vetor apenas com as variaveis, sem a base
+
 for ($i=0; $i < $_POST['restricoes']; $i++) {//pegando valores da coluna b
 	$vetor["b"]["$i"] = $_POST["b$i"];
 }
 
-$tamanhoVetor = (count($vetor));
-
 for ($i=0; $i < $_POST["variaveis"]; $i++) { //pegar valores de z
-	$vetor["z"]["$i"] = $_POST["x$i"];
+	$vetor["z"]["$i"] = $_POST["x$i"] * -1;
 }
 
 for ($i=count($vetor["z"]); $i < $tamanhoVetor; $i++) { //completar as colunas valores de Z com 0
@@ -40,32 +40,50 @@ for ($i=0; $i < $_POST['restricoes']; $i++) { //gerando as linhas
 		}
 	}	
 }
-
+//pegar coluna do pivo
 $a = min($vetor["z"]);
 
-$continuar = true;
-//while($continuar){
-
-
-	/*$vetor = array("x0" => array(1,2,-3),
-		"x1" => array(1,2,-4),
-		"x2" => array(1,2,-5),
-		"x3" => array(1,0,0),
-		"x4" => array(0,1,0));
-	for ($i=0; $i < (count($vetor)-1) ; $i++) { 
-		$vetor["z"]["$i"] = $vetor["x$i"][2];
+$coluna;
+for ($i=0; $i < $vetor["z"]; $i++) {
+	if($a == $vetor["z"]["$i"]){
+		$coluna = $i;
+		break;
 	}
+}
+//multiplicar coluna do pivo por b
+$b = array();
+for ($i=0; $i < count($vetor["b"]); $i++) { 
+	$b[$i] = $vetor["b"]["$i"] * $vetor["x$coluna"]["$i"];
+}
+//pegar linha do pivo
+$colunaB = min($b);
+$linha;
+for ($i=0; $i < $b; $i++) {
+	if($colunaB == $b["$i"]){
+		$linha = $i;
+		break;
+	}
+}
+
+$pivo["linha"] = $linha;
+$pivo["coluna"] = $coluna;
+
+
+
+
+$continuar = true;
+while($continuar){
 
 	//condição de parada por solução ótima da linha de Z
-	/*foreach ($vetor["z"] as $v => $c) {
+	foreach ($vetor["z"] as $v => $c) {
 		if($c < 0){
 			$continuar = true;
 		}else{
 			$continuar = false;
 		}
-	}*/
+	}
 
-//}
+}
 
  ?>
 
@@ -76,8 +94,12 @@ $continuar = true;
 </head>
 <body>
 <pre>
+<h1>Vetor Posicoes</h1>	
 	<?php var_dump($vetor); ?>
-<?php var_dump($a); ?>
+<h1>Vetor Base</h1>	
+	<?php var_dump($b); ?>
+<h1>Vetor Pivo</h1>	
+	<?= var_dump($pivo)?>
 
 </pre>
 
